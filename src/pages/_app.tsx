115a39b0db26098d13c9d1496080ce09b6595bbc
Administrador <administrador@MacBookPro-gonzalocastillo.local>
2025-05-25 16:30:04 -0600

    feat: :art: adding v1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/index.scss'
+import type {AppProps} from "next/app";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({Component,pageProps}: AppProps) {
+  const queryClient = new QueryClient()
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
