63fe74a7fbf70d4d99aca778ba2a483db17de276
Roger Torres <torresroger445@gmail.com>
2025-09-17 13:26:04 -0600

    adding blog route

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "trablisa-assets.s3.eu-south-2.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
